5e2d756c1ffdeba323b0eee197498ed42c4fa242
AnjithGopi <ng.anjith444@gmail.com>
2025-06-20 15:18:43 +0530

    some minor updations in user login

--- Backend/src/controllers/userController.ts
+++ Backend/src/controllers/userController.ts
@@ -53,18 +53,14 @@ export class userController {
         console.log(user);
         res.cookie("AccessToken", user.accessToken, {
           httpOnly: true,
-          //secure: process.env.NODE_ENV === "production",
-          // secure:false,
-          // sameSite: "lax",
-          sameSite:"none",
-          secure:true,
+          sameSite: "none",
+          secure: true,
           maxAge: 60 * 60 * 1000,
         });
 
         res.cookie("RefreshToken", user.refreshToken, {
           httpOnly: true,
-         // secure: process.env.NODE_ENV === "production",
-         secure:false,
+          secure: false,
           sameSite: "lax",
           maxAge: 7 * 24 * 60 * 60 * 1000,
         });
