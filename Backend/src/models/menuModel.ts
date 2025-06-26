e853244869a72e04dd0e353faf57c541d2a559ca
AnjithGopi <ng.anjith444@gmail.com>
2025-06-26 23:43:26 +0530

    menu model updated

--- Backend/src/models/menuModel.ts
+++ Backend/src/models/menuModel.ts
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const menuSchema = new mongoose.Schema({
   hotelId: { type: mongoose.Schema.ObjectId, ref: "Vendor" },
   itemName: { type: String, required: true },
+  images:[{type:String}],
   category: { type: mongoose.Schema.ObjectId, ref: "MenuCategory" },
   description: { type: String, required: true },
   quantity: { type: Number, required: true },
   isAvailable: { type: Boolean, default: true },
-  images: { type: String },
   price: { type: Number },
   availableFrom: {
     start: { type: Date },
